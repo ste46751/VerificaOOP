c902bb7ecb3c709a1c36f703e464bbc033954781
ste46751 <stefanquarti060@gmail.com>
2024-01-09 11:52:10 +0100

    conto completato

--- VerificaOOP/Program.cs
+++ VerificaOOP/Program.cs
@@ -9,28 +9,44 @@ namespace VerificaOOP
     public class Persona
     {
         private string nome;
+
+        public Persona() 
+        {
+
+            nome = "user";
+        }    
         public string Nome
         {
             get { return nome; }
-            set { 
-                    if(nome.Length>2)
-                    {
-                         nome = value;
-                    }
-                else { nome = "user"; }
+            set
+            {
+                if ( nome.Length > 2)
+                {
+                    nome = value;
                 }
+                else
+                {
+                }
+            }
         }
     }
     public class Conto : Persona
     {
         private bool chiu;
+        private float _saldo;
+
+        public Conto()
+        {
+            chiu = true;
+            _saldo = 0;
+        }
         public bool Chiu
         {
             get { return chiu; }
             set { chiu = value; }
         }
 
-        private float _saldo;
+        
         public float Saldo
         {
             get { return _saldo; }
@@ -40,14 +56,43 @@ namespace VerificaOOP
         {
             Chiu = false;
         }
-        public void conto()
+        public void chiudi()
         {
             Saldo = 0;
             Chiu = true;
         }
+        public void deposita(float c) 
+        {
+            Saldo += c;
+        }
+        public void preleva(float p)
+        {
+            if(Saldo>p)
+            {
+                Saldo -= p;
+            }
+            else
+            {
+                Console.WriteLine("Non puoi prelevare questa somma perchè il suo saldo non è sufficiente");
+            }
+
+           
+        }
         public float saldo()
         {
             return Saldo;
+        }
+
+        public string stato()
+        {
+            if(Chiu==false)
+            {
+                return "aperto";
+            }
+            else 
+            {
+                return "chiuso";
+            }
         }
         
     }
@@ -59,20 +104,92 @@ namespace VerificaOOP
 
             Console.WriteLine("Inserisci il nome");
             string n = Console.ReadLine();
-            switch(Console.ReadLine())
-            {
-
-            }
             conto.Nome = n;
-            Console.WriteLine("Inserisci il saldo");
-            float s = int.Parse(Console.ReadLine());
 
-            conto.Saldo = s;
+            bool uscita = false;
 
-            Console.WriteLine($"Il saldo di {conto.Nome}: {conto.saldo()}");
-            
+            Console.WriteLine("Menu:\n1)Conto\n2)Banca");
+            string r=Console.ReadLine();
 
+            if(r=="1" || r=="2")
+            {
+                if (r == "1")
+                {
+                    while (uscita == false)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("1) Apri conto");
+                        Console.WriteLine("2) Azzera il conto");
+                        Console.WriteLine("3) Deposita sul conto");
+                        Console.WriteLine("4) Preleva dal conto");
+                        Console.WriteLine("5) Vedi saldo sul conto");
+                        Console.WriteLine("6) Visualizza info conto");
+                        Console.WriteLine("Altri Tasti) Esci");
 
+                        switch (Console.ReadLine())
+                        {
+                            case "1":
+                                if (conto.Chiu == false)
+                                {
+                                    Console.WriteLine("Il conto è già aperto");
+                                }
+                                else if (conto.Chiu == true)
+                                {
+                                    conto.apri();
+                                    Console.WriteLine("Il conto è stato aperto");
+                                    Console.ReadLine();
+                                }
+                                break;
+
+                            case "2":
+                                conto.chiudi();
+                                Console.WriteLine("Il conto è stato chiuso");
+                                Console.ReadLine();
+                                break;
+
+                            case "3":
+                                Console.WriteLine("Inserisci la somma da depositare");
+                                float c = int.Parse(Console.ReadLine());
+                                conto.deposita(c);
+                                Console.WriteLine("Il saldo è stato incrementato");
+                                Console.ReadLine();
+                                break;
+
+                            case "4":
+                                Console.WriteLine("Inserisci la somma da prelevare");
+                                float p = int.Parse(Console.ReadLine());
+                                conto.preleva(p);
+                                Console.WriteLine("Il saldo è stato decrementato");
+                                Console.ReadLine();
+                                break;
+
+                            case "5":
+                                Console.WriteLine($"Saldo attuale: {conto.saldo()} euro");
+                                Console.ReadLine();
+                                break;
+
+                            case "6":
+                                Console.WriteLine($"Nome: {conto.Nome}\nStato conto: {conto.stato()}\nSaldo: {conto.Saldo}");
+                                Console.ReadLine();
+                                break;
+
+                            default:
+                                uscita = true;
+                                break;
+                        }
+                    }
+                }
+                else
+                {
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Puoi scegliere solo tra 1 o 2");
+                Console.WriteLine("Menu:\n1) Conto\n2) Banca");
+                r = Console.ReadLine();
+            }
             Console.ReadLine();
         }
     }
